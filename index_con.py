e2ffab752c650c954c1d6fe8b997ea5db586fc4f
huyiqing <hyqclear@gmail.com>
2017-02-12 21:36:00 +0800

    UPDATE

--- index_con.py
+++ index_con.py
@@ -2,8 +2,18 @@ import tushare as ts
 import numpy as np
 import pandas as pd
 
-#中证 新能源 000941
 #中证新兴    000964
+#中证 新能源 000941
 #信息产业    h30007
 #新材料     h30597
 #机器人     h30590
+
+index_cons_dir = '/Users/huyiqing/Desktop/capstone/index_con/'
+
+index_cons_000964 = pd.read_excel(index_cons_dir + '000964cons.xls')
+index_cons_000941 = pd.read_excel(index_cons_dir + '000941cons.xls')
+index_cons_h30007 = pd.read_excel(index_cons_dir + 'h30007cons.xls')
+index_cons_h30597 = pd.read_excel(index_cons_dir + 'h30597cons.xls')
+index_cons_h30590 = pd.read_excel(index_cons_dir + 'h30590cons.xls')
+
+# print(index_cons_h30590)
